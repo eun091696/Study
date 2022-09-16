4c2339feac44a86fe4c4ebd2c0a2aafe54a88ebd
Changeun An <eun091696@gmail.com>
2022-09-16 18:17:22 +0900

    JSP Day04

---
+++ JSPDay04/src/main/java/com/jspex/beans/DBconnecter.java
@@ -0,0 +1,26 @@
+package com.jspex.beans;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBconnecter {
+	public static Connection getConnection() {
+		Connection conn = null;
+		
+		try {
+			String url = "jdbc:mariadb://localhost:3307/tbl_member";
+			String dbID = "ace";
+			String dbPassword = "1234";
+			Class.forName("org.mariadb.jdbc.Driver");
+			conn = DriverManager.getConnection(url, dbID, dbPassword);
+		} catch(ClassNotFoundException e) {
+			System.out.println("드라이버 로딩 실패");
+		} catch(SQLException e) {
+			System.out.println("DB 접속 실패");
+		} catch(Exception e) {
+			System.out.println("알 수 없는 오류" + e);
+		}
+		return conn;
+	}
+}
