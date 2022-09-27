e84d9f57c2ad0bc16225d49d97e0a93810a51093
Changeun An <eun091696@gmail.com>
2022-09-27 23:26:20 +0900

    HTML, js를 이용하여 이름, 주소, 연락처의 입력값을 받아 입력받은 순서대로 테이블 생성

--- JavaScript/js/innerHtml.js
+++ JavaScript/js/innerHtml.js
@@ -1,14 +1,39 @@
 const button = document.querySelector("button");
-const inputs = document.querySelectorAll("input");
+
+let userList = new Array();
 
 button.onclick = () => {
-    for(let i = 0; i < 4; i++){
-        document.getElementById("test").innerHTML = "<tr>" +
-            "<td>" + (i + 1) + "</td>" +
-            "<td>" + 2+ "</td>" +
-            "<td>" + 3 + "</td>" +
-            "<td>" + 4 + "</td>" +
-        "</tr>"
-        break;
-        }
+  addUser();
+  loadList();
+}
+
+function addUser() { //유저 정보 입력
+  const inputs = document.querySelectorAll("input");
+
+  let user = {
+    name2: inputs[0].value,
+    address: inputs[1].value,
+    phone: inputs[2].value
+  }
+
+  userList.push(user);
+
+}
+
+function loadList() { //리스트 만들기
+  const tbody = document.querySelector("tbody");
+
+  tbody.innerHTML = ""; //tbody의 HTML값 초기화
+
+  userList.forEach((user, index) => {
+    tbody.innerHTML += `
+      <tr>
+        <td>${index}</td>
+        <td>${user.name2}</td>
+        <td>${user.address}</td>
+        <td>${user.phone}</td>
+      </tr>
+      `
+  });
+  
 }
